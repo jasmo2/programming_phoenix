0d55c6f8572a60ecc1281e125422f189c88f741d
jasmo2 <jasmo2.com@gmail.com>
2016-06-13 10:57:48 -0500

    plug before action

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -1,11 +1,11 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
-
+  plug(:authenticate when action in [:index , :show])
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
-  end
+     users = Repo.all(User)
+     render conn, "index.html", users: users
+ end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
@@ -28,4 +28,14 @@ defmodule Rumbl.UserController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+  defp authenticate(conn) do
+   if conn.assigns.current_user do
+     conn
+   else
+     conn
+     |> put_flash(:error, "You must be logged in to access that page")
+     |> redirect(to: page_path(conn, :index))
+     |> halt()
+   end
+ end
 end
