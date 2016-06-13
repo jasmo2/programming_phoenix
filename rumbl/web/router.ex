f0810c61186419305565e22e61cb07c7136b7a1f
jasmo2 <jasmo2.com@gmail.com>
2016-06-13 09:56:26 -0500

    add users resources and get user by username

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -17,6 +17,9 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:create, :index, :new]
+    get "/users/:username", UserController, :show
+    # resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
