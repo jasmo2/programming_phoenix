ba156f06e7a165e305557cfaec2fee1a9f231008
jasmo2 <jasmo2.com@gmail.com>
2016-06-13 13:02:36 -0500

    fix  authenticate private function

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -1,11 +1,12 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
-  plug(:authenticate when action in [:index , :show])
+  # plug :authenticate when action in [:index , :show]
+
   def index(conn, _params) do
-     users = Repo.all(User)
-     render conn, "index.html", users: users
- end
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
+  end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
@@ -24,18 +25,19 @@ defmodule Rumbl.UserController do
         conn
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        {:error, changeset} ->
+          render(conn, "new.html", changeset: changeset)
+        end
     end
-  end
-  defp authenticate(conn) do
-   if conn.assigns.current_user do
-     conn
-   else
-     conn
-     |> put_flash(:error, "You must be logged in to access that page")
-     |> redirect(to: page_path(conn, :index))
-     |> halt()
-   end
- end
+    
+    defp authenticate(conn) do
+      if conn.assigns.current_user do
+        conn
+      else
+        conn
+        |> put_flash(:error, "You must be logged in to access that page")
+        |> redirect(to: page_path(conn, :index))
+        |> halt()
+      end
+    end
 end
