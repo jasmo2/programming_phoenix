cdb30f40918995a2baa193a3a4c09d3b1f9ac815
jasmo2 <jasmo2.com@gmail.com>
2016-06-24 11:38:11 -0500

    add "watch_controller.ex"

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -18,14 +18,13 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/users", UserController, only: [:create, :index, :new]
-    get "/users/:username", UserController, :show
+    resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   scope "/manage", Rumbl do
-   pipe_through [:browser, :authenticate_user]
+    pipe_through [:browser, :authenticate_user]
 
-   resources "/videos", VideoController
+    resources "/videos", VideoController
   end
 end
