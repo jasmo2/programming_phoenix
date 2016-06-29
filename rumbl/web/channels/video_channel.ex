bed6a7b4e3c6b51669a41891ea73cd6888c7aedd
jasmo2 <jasmo2.com@gmail.com>
2016-06-29 10:44:59 -0500

    socket communication

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -1,7 +1,14 @@
 defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
   def join("videos_c:" <> video_id,  _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id, 10))}
-    
+    {:ok, socket}
+  end
+  def handle_info(message, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"}
+      body: params["body"]
+      at: params["at"]
+    })
+    {:reply, :ok, socket}
   end
 end
--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -16,11 +16,38 @@ let Video = {
     let msgInput     = document.getElementById("msg-input")
     let postButton   = document.getElementById("msg-submit")
     let vidChannel   = socket.channel("videos_c:" + videoId)
-
+    
+    postButton.addEventListener('click', e => {
+      let payload = {}
+      vidChannel.push("new_annotation", payload)
+      .receive("error", e => console.log(e))
+      msgInput.value = ""
+    })
     // join to VideoChannel
     vidChannel.join()
-      .receive("ok", resp => console.log("joined video channel", resp ))
-      .receive("error", resp => console.log("error: ", resp))
+    .receive("ok", resp => console.log("joined video channel", resp ))
+    .receive("error", resp => console.log("error: ", resp))
+
+    vidChannel.on("new_annotation", (response) =>  {
+      this.renderAnnotation(msgContainer, response)
+    })
+  },
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    let template = document.createElement("div")
+
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+    <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 export default Video
