f36bc3c21a4a3de87384af86c4131371c82f55fa
jasmo2 <jasmo2.com@gmail.com>
2016-07-06 12:09:39 -0500

    refactor video_channel, proper handle annotation

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -9,40 +9,37 @@ defmodule Rumbl.VideoChannel do
     video_id = String.to_integer(video_id)
     video = Repo.get!(Video, video_id)
     annotations = Repo.all(
-     from a in assoc(video, :annotations),
+     from( a in assoc(video, :annotations),
        order_by: [asc: a.at, asc: a.id],
        limit: 200,
        preload: [:user]
-   )
+   ))
 
     resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView, "annotation.json")}
     {:ok, resp,  assign(socket, :video_id, video_id)}
   end
 
 
-  def handle_in(event, params, socket) do
-    user = Repo.get(User, socket.assings.user_id)
-    handle_in(event, params, user, socket)
-  end
+  def handle_in("new_annotation", params, socket) do
+     user = Rumbl.Repo.get(User, socket.assigns.user_id)
 
+     changeset =
+       user
+       |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+       |> Annotation.changeset(params)
 
-  def handle_in("new_annotation", params, user, socket) do
-    changeset =
-      user
-      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
-      |> Annotation.changeset(params)
+     case Repo.insert(changeset) do
+       {:ok, annotation} ->
+         broadcast! socket, "new_annotation", %{
+           id: annotation.id,
+           user: Rumbl.UserView.render("user.json", %{user: user}),
+           body: annotation.body,
+           at: annotation.at
+         }
+         {:reply, :ok, socket}
 
-    case Repo.insert(changeset) do
-      {:ok, annotation} ->
-        broadcast!(socket, "new_annotation", %{
-          user: %{username: "anon"},
-          body: params["body"],
-          at: params["at"]
-          })
-          {:reply, :ok, socket}
-
-      {:error, changeset} ->
-        {:reply, {:error, %{errors: changeset}}, socket}
-    end
-  end
+       {:error, changeset} ->
+         {:reply, {:error, %{errors: changeset}}, socket}
+     end
+   end
 end
